d2a21e050854dbf6f0e2450c04ec7f2e59ef4560
Priyanshu Singh <singhpriyanshu144@gmail.com>
2026-02-15 23:40:44 +0530

    add mongodb

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express');
-app = express();
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
- res.send("Hello priyanshu")
+const app = express();
+
+mongoose.connect("mongodb://mongo-db:27017/my-database")
+.then(() => {
+    console.log("Connection successful");
+
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
 })
-app.listen(3000, () => {
- console.log('Server is running on port 3000');
+.catch((err) => {
+    console.log("Database connection error:", err);
 });
